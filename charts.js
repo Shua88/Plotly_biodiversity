896a04975a3a005f32b314e116b08151d273df7c
Shua88 <118570717+Shua88@users.noreply.github.com>
2023-03-08 22:51:36 -0600

    Add files via upload

--- charts.js
+++ charts.js
@@ -65,20 +65,26 @@ function buildCharts(sample) {
     // console.log(samples)
     // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    
+    // console.log(resultArray)  
     // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata = data.metadata
+    
+    var metaArray = metadata.filter(sampleMeta => sampleMeta.id == sample);
 
+  // console.log(metaArray)
     // Deliverable 1: 5. Create a variable that holds the first sample in the array.
     var result = resultArray[0];
     // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
-
+    var metaresult = metaArray[0];
     // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = result.otu_ids
     var otu_labels = result.otu_labels
     var sample_values = result.sample_values
   
     // Deliverable 3: 3. Create a variable that holds the washing frequency.
-    // var 
+    var wfreq = metaresult.wfreq
+
+    // console.log(wfreq)
 
     // Deliverable 1: 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order 
@@ -124,10 +130,18 @@ function buildCharts(sample) {
     // Deliverable 2: 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
     // Deliverable 3: 4. Create the trace for the gauge chart.
-    
+    var gaugeData = [{
+      domain: { x: [0, 10], y: [0, 10] },
+		  value: wfreq,
+		  title: { text: "Belly Button Washing Frequency" },
+		  type: "indicator",
+		  mode: "gauge+number"
+
+    }]
+
     // Deliverable 3: 5. Create the layout for the gauge chart.
-
+    var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
     // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-
+    Plotly.newPlot('Gauge', gaugeData, layout);
   });
 }
